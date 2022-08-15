1be766c87a6388f19ebc8f3e32b1af86def0a0dc
Brandon Lee <brandon5959somi@gmail.com>
2022-08-15 22:17:43 +0900

    - webAppProject mybatis 설정 및 테스트코드 작성 완료

---
+++ src/main/java/com/dev/webApp/mapper/TestMapper.java
@@ -0,0 +1,4 @@
+package com.dev.webApp.mapper;
+
+public interface TestMapper {
+}
---
+++ src/test/java/com/dev/webApp/mybatis/MybatisTests.java
@@ -0,0 +1,23 @@
+package com.dev.webApp.mybatis;
+
+import com.dev.webApp.mapper.TestMapper;
+import lombok.extern.log4j.Log4j;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@Log4j
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration("file:src/main/webapp/WEB-INF/spring/root-context.xml")
+public class MybatisTests {
+
+    @Autowired
+    private TestMapper testMapper;
+
+    @Test
+    public void testMapper() {
+        log.info(testMapper.getClass().getName());
+    }
+}
