1573aaf7ceda2966a83be4577ef8d72691576ac5
Brandon Lee <brandon5959somi@gmail.com>
2022-08-15 21:38:48 +0900

    - webAppProject 1) mybatis 설정 추가 완료 2) sqlSeesionFactory 설정 추가 완료

--- src/test/java/com/dev/webApp/persistence/DBConnectionTest.java
+++ src/test/java/com/dev/webApp/persistence/DBConnectionTest.java
@@ -1,5 +1,8 @@
 package com.dev.webApp.persistence;
 
+import lombok.extern.log4j.Log4j;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,11 +11,11 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import javax.sql.DataSource;
 import java.sql.Connection;
-import java.sql.SQLException;
 
+@Log4j
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration("file:src/main/webapp/WEB-INF/spring/root-context.xml")
-public class DataSourceTest {
+public class DBConnectionTest {
 
     // todo: 추후 아래 문서 보고 추가할 내용 있으면 추가. connection pool 설정하는법 나와잇음
     // https://kimvampa.tistory.com/57
@@ -20,11 +23,23 @@ public class DataSourceTest {
     @Autowired
     private DataSource dataSource;
 
+    @Autowired
+    private SqlSessionFactory sqlSessionFactory;
+
     @Test
-    public void testConnection() throws SQLException {
+    public void testDBConnection() throws Exception {
 
         Connection connection = dataSource.getConnection();
 
         System.out.println("connection="+connection);
+    }
+
+    @Test
+    public void testMyBatisConnection() throws Exception {
+        SqlSession session = sqlSessionFactory.openSession();
+        Connection connection = session.getConnection();
+
+        log.info("session="+session);
+        log.info("connection="+connection);
     }
 }
--- src/test/java/com/dev/webApp/persistence/JDBCconnectionTests.java
+++ src/test/java/com/dev/webApp/persistence/JDBCconnectionTests.java
@@ -6,7 +6,7 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
-public class JDBCTests {
+public class JDBCconnectionTests {
 
     static {
         try {
