0d73aabe23dc2b974e0985119f2396c9a866e313
Brandon Lee <brandon5959somi@gmail.com>
2022-08-22 23:34:18 +0900

    - webAppProject jsp전까지 c/r/u/d 처리 중...

--- src/main/java/com/dev/webApp/controller/NoticeController.java
+++ src/main/java/com/dev/webApp/controller/NoticeController.java
@@ -1,7 +1,73 @@
 package com.dev.webApp.controller;
 
-import org.springframework.web.bind.annotation.RestController;
+import com.dev.webApp.domain.dto.InsertNoticeDTO;
+import com.dev.webApp.domain.vo.NoticeVO;
+import com.dev.webApp.service.NoticeService;
+import com.mysql.cj.protocol.x.Notice;
+import lombok.RequiredArgsConstructor;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+@RequestMapping("/notice")
+@RequiredArgsConstructor
 @RestController
 public class NoticeController {
+
+    private NoticeService noticeService;
+
+    @GetMapping("/content/list")
+    public void selectNoticeList(Model model) {
+
+        // 우선은 1000개로 지정하겠습니다.
+
+        InsertNoticeDTO insertNoticeDTO = InsertNoticeDTO.builder()
+                .manyNoticeOrNot(false)
+                .noticeSize(1000)
+                .build();
+
+        model.addAttribute("list", noticeService.getNoticeList(insertNoticeDTO));
+    }
+
+
+    @GetMapping("/content")
+    public void getContent(
+            @RequestParam
+            Long noticeNo
+            , Model model
+    ) {
+
+        NoticeVO noticeVO = NoticeVO.builder()
+                .noticeNo(noticeNo)
+                .build();
+
+        model.addAttribute("board", noticeService.getNotice(noticeVO));
+    }
+
+    @PostMapping("/content")
+    public String postNotice(NoticeVO noticeVO, RedirectAttributes redirectAttributes) throws Exception {
+
+        noticeService.registerNotice(noticeVO);
+
+        redirectAttributes.addFlashAttribute("result", noticeVO.getNoticeNo());
+
+        // 내부적으로 response.sendRedirect를 처리해주게끔 처리합니다.
+        return "redirect:/post/list";
+    }
+
+    @PutMapping("/content")
+    public String putNotice(NoticeVO noticeVO, RedirectAttributes redirectAttributes) throws Exception {
+
+        noticeService.modifyNotice(noticeVO);
+
+        return "redirect:/notice/list";
+    }
+
+    @DeleteMapping("/content")
+    public String deleteNotice(Long noticeNo, RedirectAttributes redirectAttributes) throws Exception {
+
+        noticeService.removeNotice(noticeNo);
+
+        return "redirect:/notice/list";
+    }
 }
--- src/main/java/com/dev/webApp/domain/vo/NoticeVO.java
+++ src/main/java/com/dev/webApp/domain/vo/NoticeVO.java
@@ -1,12 +1,14 @@
 package com.dev.webApp.domain.vo;
 
 import com.dev.webApp.util.NoticeUseYnEnum;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import java.util.Date;
 
 @Data
+@Builder
 @NoArgsConstructor
 public class NoticeVO {
 
