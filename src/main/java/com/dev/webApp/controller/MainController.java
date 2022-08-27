dbbcfb5dcb55dfc7498c0546b4b43af868244439
Brandon Lee <brandon5959somi@gmail.com>
2022-08-28 00:22:12 +0900

    - webAppProject Controller test 초안 완성

--- src/main/java/com/dev/webApp/controller/MainController.java
+++ src/main/java/com/dev/webApp/controller/MainController.java
@@ -1,4 +1,19 @@
 package com.dev.webApp.controller;
 
+// RestController Test
+
+import lombok.extern.log4j.Log4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/main")
 public class MainController {
+
+    @GetMapping(value = "/getText", produces = "text/plain; charset=UTF-8")
+    public String getText() {
+
+        return " 안녕하세요";
+    }
 }
---
+++ src/test/java/com/dev/webApp/controller/MainControllerTest.java
@@ -0,0 +1,46 @@
+package com.dev.webApp.controller;
+
+import junit.framework.TestCase;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@RunWith(SpringJUnit4ClassRunner.class) // test for using junit4
+@ContextConfiguration({
+        "file:src/main/webapp/WEB-INF/spring/root-context.xml"
+        ,"file:src/main/webapp/WEB-INF/spring/appServlet/servlet-context.xml"
+})
+@WebAppConfiguration // Test for controller
+public class MainControllerTest extends TestCase {
+
+    @Autowired
+    private WebApplicationContext ctx;
+
+    private MockMvc mockMvc;
+
+    @Before
+    public void init() {
+        this.mockMvc = MockMvcBuilders.webAppContextSetup(ctx).build();
+    }
+
+    @Test
+    public void testConvert() throws Exception {
+
+        //
+        MvcResult mvcResult = mockMvc.perform
+                (get("/main/getText"))
+                .andExpect(status().is(200))
+                .andReturn();
+    }
+}
