c62cf1a9227a2bbc626f8e1ad372c01ae3b2272e
BrandonLee <brandon5959somi@gmail.com>
2022-10-10 17:08:41 +0900

    - webAppProject 1)pom.xml에 transaction관련 코드추가 2)transaction 관련 설정 중

--- src/test/java/com/dev/webApp/service/BlogServiceTests.java
+++ src/test/java/com/dev/webApp/service/BlogServiceTests.java
@@ -6,12 +6,10 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
 import static org.junit.Assert.assertNotNull;
-import static org.mockito.Mockito.mock;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration("file:src/main/webapp/WEB-INF/spring/root-context.xml")
